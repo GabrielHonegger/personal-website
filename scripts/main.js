a39a9006117ae34e4a35aa1d4c504fbdb2162548
GabrielHonegger <gabrielhonegger132@gmail.com>
2024-03-29 12:19:42 -0300

    building responsive menu

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,17 @@
 const hamburgerMenu = document.querySelector('.hamburger');
 const menuItens = document.querySelector('.menu');
+const hamburgerImage = document.querySelector('.hamburger-image');
+
+let activeIcon = "/img/cardapio.png";
 
 hamburgerMenu.addEventListener('click', () => {
     menuItens.classList.toggle('is-active');
+    console.log(activeIcon);
+    if (activeIcon === "/img/cardapio.png") {
+        hamburgerImage.src = "/img/botao-fechar.png";
+        activeIcon = "/img/botao-fechar.png";
+    } else {
+        hamburgerImage.src = "/img/cardapio.png";
+        activeIcon = "/img/cardapio.png";
+    }
 })
